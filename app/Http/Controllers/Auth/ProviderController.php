fa24e0f79da792397056f44c167ac931fb0667b1
MridulKar <mkarpratyay@gmail.com>
2023-06-16 23:11:30 +0600

    github

---
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class ProviderController extends Controller
+{
+    public function redirect($provider){
+        return Socialite::driver($provider)->redirect();
+    }
+    public function callback($provider){
+
+        $socialUser = Socialite::driver($provider)->user();
+
+        $user = User::updateOrCreate([
+            'provider_id' => $socialUser->id,
+            'provider'    => $provider
+        ], [
+            'username' => $socialUser->nickname,
+            'email' => $socialUser->email,
+            'provider_token' => $socialUser->token,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+}
